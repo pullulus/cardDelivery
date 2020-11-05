9c41092b01bf50bf421cc937f07cfdbb7393f25a
Elena Tsypileva <etsypileva@gmail.com>
2020-11-05 18:40:16 +0300

    changed DeliveryTest.java

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -5,29 +5,27 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static java.time.LocalDate.*;
 
 public class DeliveryTest {
+
     @Test
     void shouldRunDeliveryForm() {
         open("http://localhost:9999");
         $("[placeholder= 'Город']").setValue("Кострома");
+        $("[placeholder= 'Дата встречи']").doubleClick();
         $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        $("[placeholder= 'Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE));
-        LocalDate date = LocalDate.now().plusDays(3);
-        $("[placeholder= 'Дата встречи']").
-        $$("[type= 'text']").last().setValue("Сусанин Иван");
+        String dateOfMeeting = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder= 'Дата встречи']").setValue(dateOfMeeting);
+        $("[name= 'name']").setValue("Сусанин Иван");
         $("[name= 'phone']").setValue("+78889990001");
         $(".checkbox__box").click();
         $(".button__content").click();
-        $(".notification").waitUntil(Condition.visible, 15000);
+        $(".notification").waitUntil(visible, 15000);
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + dateOfMeeting));
     }
 }
